66a86b50c3214fa688c569bbbb8ccd47712956e6
WillDobleH <willtrevor888x3h@gmail.com>
2026-04-24 20:34:56 -0600

    Delete read_docx.ps1

--- read_docx.ps1
+++
@@ -1,9 +0,0 @@
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-$zip = [System.IO.Compression.ZipFile]::OpenRead('C:\Users\Hegel\Desktop\Agua\PoliticasGarantia_AguaVidaSaludable.docx')
-$entry = $zip.GetEntry('word/document.xml')
-$stream = $entry.Open()
-$reader = New-Object System.IO.StreamReader($stream)
-$xml = $reader.ReadToEnd()
-$reader.Close()
-$zip.Dispose()
-$xml -replace '<[^>]+>', ' ' -replace '\s+', ' '
